b17ef30ad54fd27954b835c028561052282d3219
philipp <67582054+philipp-oe@users.noreply.github.com>
2022-11-30 16:10:17 +0100

    Add files via upload

---
+++ 3_1D_parabolic_PDEs.m
@@ -0,0 +1,162 @@
+% Program for solving Parabolic PDE's in 1D with explicit Euler,
+% Method of Lines and comparison to Matlab's build in functions
+% du/dt = Au + b; u(0) = 0
+%
+% as of 03st Nov. 2022
+
+clearvars
+%% Parameters
+L = 1;      Tmax = 2;
+Nx = 50;    hx = L/Nx 
+% ht = hx^2; Nt = floor(Tmax/ht);
+
+Nt = 5000;  % stable solution
+%Nt = 2900;  % unstable solution
+ht =Tmax/Nt
+
+t = [0:ht:Tmax];    x = [0:hx:L];
+u0 = zeros(Nx+1,1);
+
+%% solving with explicit Euler and Method of lines
+sol = mol_sol(Nx,hx,Nt,ht);
+
+u0t = zeros(Nt+1,1);
+for i=1:Nt/2
+    u0t(i) = sin(pi*t(i)); end
+
+sol = [u0t sol];
+
+% line below plots the figures for part a
+plot1(sol,x,t,Nt)
+
+%% Comparison with the MATLAB inbuild function ode23(s/sJ)
+% parameters
+N=400       
+hx = L/N;
+a=0.3;      
+c1 = a/hx^2;
+tspan = [0 Tmax];
+u0 = zeros(1,N);        u(:,1) = u0;
+
+A=gallery('tridiag',N,1,-2,1);
+A(N,N-1) = 2;
+A = c1*A;
+
+% comutation ode23
+opts = odeset('RelTol',1e-4,'AbsTol',1e-8);
+tic;
+[t u] = ode23(@(t,u) rhs_heat(u,A,t,N,c1),tspan, u0,opts);
+u0t = zeros(length(t),1);
+i = 1;
+while t(i) <1
+    u0t(i) = sin(pi*t(i)); 
+    i = i+1;
+end
+u = [u0t u];    % adding the boundary condtions for u(0,t)
+
+disp('ode23')
+timesteps = length(t)
+compute_time = toc 
+
+%computation ode23s
+tic;
+[t u] = ode23s(@(t,u) rhs_heat(u,A,t,N,c1),tspan, u0,opts);
+u0t = zeros(length(t),1);
+i = 1;
+while t(i) <1
+    u0t(i) = sin(pi*t(i)); 
+    i = i+1;
+end
+u = [u0t u];    % adding the boundary condtions for u(0,t)
+
+disp('ode23s')
+timesteps = length(t)
+compute_time = toc 
+
+%comtutation ode23s Jacobian 
+opts = odeset(opts,'Jacobian',A);
+tic;
+[t u] = ode23s(@(t,u) rhs_heat(u,A,t,N,c1),tspan, u0,opts);
+u0t = zeros(length(t),1);
+i = 1;
+while t(i) <1
+    u0t(i) = sin(pi*t(i)); 
+    i = i+1;
+end
+u = [u0t u];    % adding the boundary condtions for u(0,t)
+
+disp('ode23sJ')
+timesteps = length(t)
+compute_time = toc
+
+% remove comment for plot
+%{
+u1 = u(:,1)';
+x=[0:hx:L];
+[X T] =meshgrid(x,t);
+figure 
+mesh(X,T,u)
+xlabel('x \rightarrow')
+ylabel('\leftarrow t')
+zlabel('Temp. (arb. unit) \rightarrow')
+%}
+
+%% functions
+function sol = mol_sol(Nx,hx,Nt,ht)
+% solves du/dt = rhs_heat with explicit euler, as part of methods of lines
+N=Nx;
+a= 0.3;
+c1 = a/hx^2;
+
+u0 = zeros(N,1);
+u(:,1) = u0;
+
+A=gallery('tridiag',N,1,-2,1);
+A(N,N-1) = 2;
+
+A = c1*A;
+
+% Euler Method u(x,t+1) = u(x,t) + ht( A*u(x,t) + b(t))
+for i = 1:Nt
+    t = (i-1)*ht;
+    u(:,i+1) = u(:,i) + ht*( rhs_heat(u(:,i),A,t,N,c1) );
+end
+
+sol =[u]';
+
+end
+
+function fun=rhs_heat(u,A,t,N,c1)
+% right hand side of the PDE du/dt = a*d^2u/dt^2
+    b=zeros(N,1);
+    if t <= 1;
+        b(1) = c1*sin(pi*t); end
+    fun = A*u+b;
+end
+
+function plot1(sol,x,t,Nt)
+    % 3D Plot 
+    [X T] = meshgrid(x,t);
+
+    figure
+    mesh(X,T,sol);
+    xlabel('x \rightarrow')
+    ylabel('\leftarrow t')
+    zlabel('Temp. (arb. unit) \rightarrow')
+
+    % 2D Plot at t=1
+    t1 = Nt/2;
+    u_t1 = sol(t1,:);
+
+    figure
+    plot(x,u_t1,'r*')
+    xlabel('x \rightarrow')
+    ylabel('Temp. (arb. unit) \rightarrow')
+
+    figure 
+    plot(t,sol(:,1),'r',t,sol(:,end),'k')
+    xlabel('t \rightarrow')
+    ylabel('Temp. (arb. unit) \rightarrow')
+    legend('left side', 'right side')
+
+end
---
+++ 3_2D_parabolic_PDEs.m
@@ -0,0 +1,156 @@
+% Program for 2D parabolic PDE solving with Crank Nicelson method
+% time-dependent solution of heat flow through a solid metall block 
+% du/dt = laplace u + f
+%
+% as of 9th Nov. 2022 
+
+clearvars 
+close all
+%% Parameters and main
+
+% for animation set q = 0 and chose h = 0.1
+q = 1;
+
+Lx = 10;    Ly = 6;     L = [Lx Ly];
+h = 0.05; ht = h;
+M = Lx/h; N = Ly/h;      % #total inner points (w/o ghost points)
+M = M+1;
+Tmax = 50;
+tsteps = Tmax/ht;
+T=[0:ht:Tmax];
+
+% Matrix Implementation
+A = getA(M,N,h);
+k = length(A);
+B = sparse(eye(k) - ht/2*A);
+C = sparse(eye(k) + ht/2*A);
+clear k
+
+fun = get_f(L(1),L(2),h);
+
+T0 = 20*ones(M,N+1)';
+temps = cell(4,1);
+temps{1} = T0;
+k = 20*ones(1,M);
+
+u = reshape(T0(2:end,:),[],1);
+for i=2:tsteps+1
+    u_next = cn_step(B,C,h,u,fun);
+    u = u_next;
+    % saveing t=1,10,50
+    if i==(tsteps/50)
+        temps{2} = [k; reshape(u,M,N)'];
+    elseif i==(tsteps/5)
+        temps{3} = [k; reshape(u,M,N)'];
+    elseif i==tsteps
+        temps{4} = [k; reshape(u,M,N)'];
+    end
+
+    %saving all (5,3) points for all t 
+    uu = [k; reshape(u,M,N)'];
+    t53(i-1) = uu((N+2)/2,(M+1)/2);
+     
+    % saving all time stept for the animation
+    if q==1
+        temps{i,1} = [k; reshape(u,M,N)']; end
+end  
+
+% plots for t=0,1,10,50
+plots(L,h,temps)
+
+t53 = [20 t53];
+%plots temperature at(5,3) over time
+figure('Name','plot1')
+plt1 = plot(T,t53);
+hold on
+datatip(plt1,'DataIndex',length(t53), ...
+    'location','northwest');
+xlabel('t \rightarrow')
+ylabel('Temp. \rightarrow')
+hold off
+
+%% Animation for t=0...50
+if q == 1
+    figure 
+    [X,Y] = meshgrid([0:h:Lx],[0:h:Ly]);
+    for i=1:length(temps)
+        mesh(X,Y,temps{i})
+        axis([0 10 0 6 20 40])
+        xlabel('x \rightarrow')
+        ylabel('y \rightarrow')
+        zlabel('Temp. \rightarrow')
+        pause(0.05)
+    end
+end
+
+%% function for A Matrix 
+function A = getA(M,N,h)
+
+    SM = gallery('tridiag',M,1,-2,1);
+    SM(1,2) = 2;
+    SM(end,end-1) = 2;
+    SM = SM/h^2;
+
+    SN = gallery('tridiag',N,1,-2,1);
+    SN(end,end-1) = 2;
+    SN = SN/h^2;
+
+    A = sparse( kron(eye(N),SM) + kron(SN,eye(M)));
+end
+%% Funtion for crank nicelson
+function step = cn_step(B,C,ht,u,fun)
+    step = B\(C*u + ht*fun);
+end
+%% Function f
+function fun = get_f(Lx,Ly,h)
+    T0 = 20;
+    x = [0:h:Lx];
+    y = [h:h:Ly];
+
+    for i=1:length(x)
+        for j=1:length(y)
+            f(i,j) = 20*exp(-0.1*(x(i)-3)^2 -2*(y(j)-1)^2);
+        end
+    end
+    f(:,1) = f(:,1) + T0/h^2;
+    fun =reshape(f,[],1);
+end
+
+%% Plot function
+function plots(L,h,temps)
+
+[X,Y] = meshgrid([0:h:L(1)],[0:h:L(2)]);
+
+figure 
+mesh(X,Y,temps{1}) 
+axis([0 10 0 6 20 40])
+xlabel('x \rightarrow')
+ylabel('y \rightarrow')
+zlabel('Temp. \rightarrow')
+
+
+figure 
+mesh(X,Y,temps{2}) 
+axis([0 10 0 6 20 40])
+xlabel('x \rightarrow')
+ylabel('y \rightarrow')
+zlabel('Temp. \rightarrow')
+
+
+figure
+mesh(X,Y,temps{3})  
+axis([0 10 0 6 20 40])
+xlabel('x \rightarrow')
+ylabel('y \rightarrow')
+zlabel('Temp. \rightarrow')
+
+
+figure 
+mesh(X,Y,temps{4}) 
+axis([0 10 0 6 20 40])
+xlabel('x \rightarrow')
+ylabel('y \rightarrow')
+zlabel('Temp. \rightarrow')
+
+
+end
